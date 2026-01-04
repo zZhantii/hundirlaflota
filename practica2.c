939630fa2af811d8fba1f451d7800ede250699cf
SantiagooLozada <santiagoolozadab@gmail.com>
2026-01-04 09:07:28 +0100

    tablero de barcos casi finalizar

--- practica2.c
+++ practica2.c
@@ -24,19 +24,63 @@ void printar_tablero(int tablero[10][10])
     }
 }
 
-void tablero_barcos(int tablero_conBarcos[10][10], struct barco flota[], int num_barcos) {
-    srand(time(NULL));
+void tablero_barcos(int tablero_conBarcos[10][10], struct barco flota[], int num_barcos)
+{
+    
 
     for (int i = 0; i < num_barcos; i++)
     {
         for (int j = 0; j < flota[i].cantidad; j++)
         {
-            int fila = rand() % 10;
-            int columna = rand() % 10;
+            int correctoFila = 0;
+            int correctoColumna = 0;
 
-            tablero_conBarcos[fila][columna] = flota[i].casillas;
+            int fila = 0;
+            int columna = 0;
+            int orientacion = 0;
+            do
+            {
 
-            printf(" Colocado %s en %c%d(tamaño %d)\n ", flota[i].nombre, 'A' + columna, fila + 1, flota[i].casillas);
+                fila = rand() % 10;
+                columna = rand() % 10;
+                orientacion = rand() % 2; // 0 = horizontal, 1 = vertical
+
+                if (orientacion == 1)
+                {
+                    if (fila + flota[i].casillas <= 10)
+                    {
+                        correctoFila = 1;
+                    }
+                }
+                else
+                {
+                    if (columna + flota[i].casillas <= 10)
+                    {
+                        correctoColumna = 1;
+                    }
+                }
+
+            } while (correctoFila == 0 && correctoColumna == 0);
+
+            if (orientacion == 0)
+            {
+                // Horizontal
+                for (int k = 0; k < flota[i].casillas; k++)
+                {
+                    if (tablero_conBarcos[][] != 0) {
+                        
+                    }
+                    tablero_conBarcos[fila][columna + k] = 1;
+                }
+            }
+            else
+            {
+                // vertical
+                for (int p = 0; p < flota[i].casillas; p++)
+                {
+                    tablero_conBarcos[fila + p][columna] = 1;
+                }
+            }
         }
     }
 
@@ -53,38 +97,34 @@ void tablero_barcos(int tablero_conBarcos[10][10], struct barco flota[], int num
     }
 }
 
-int main(void){
-
-    
+int main(void)
+{
+    srand(time(NULL));
 
     struct barco flota1[] = {
         {"portaavions", 1, 5},
         {"cuirassat", 1, 4},
         {"destructors", 2, 3},
         {"fragates", 2, 2},
-        {"submarins", 3, 1}
-    };
+        {"submarins", 3, 1}};
 
     struct barco flota2[] =
-    {
-        {"cuirassat", 1, 4},
-        {"destructors", 2, 3},
-        {"fragates", 3, 2},
-        {"submarins", 4, 1}
-    };
+        {
+            {"cuirassat", 1, 4},
+            {"destructors", 2, 3},
+            {"fragates", 3, 2},
+            {"submarins", 4, 1}};
 
     struct barco flota3[] =
-    {
-        {"destructors", 2, 3},
-        {"fragates", 3, 2},
-        {"submarins", 4, 1}
-    };
+        {
+            {"destructors", 2, 3},
+            {"fragates", 3, 2},
+            {"submarins", 4, 1}};
 
     int tablero[10][10] = {0};
     int tablero_conBarcos[10][10] = {0};
 
     int num_flota1 = sizeof(flota1) / sizeof(flota1[0]);
-    
 
     // printar_tablero(tablero);
     tablero_barcos(tablero_conBarcos, flota1, num_flota1);
@@ -115,7 +155,7 @@ int main(void){
     //         scanf("%d", &intValue);
     //         if (intValue >= 1 && intValue <= 3)
     //         {
-                
+
     //             hecho = 1;
     //         }
     //         else
@@ -129,7 +169,4 @@ int main(void){
     // {
     //     /* code */
     // }
-    
-
-    
 }

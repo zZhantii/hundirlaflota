e434f44f0e46575c6f95a61cb6ee104b8b514d99
SantiagooLozada <santiagoolozadab@gmail.com>
2026-01-09 13:50:36 +0100

    menus

--- practica2.c
+++ practica2.c
@@ -26,59 +26,74 @@ void printar_tablero(int tablero[10][10])
 
 void tablero_barcos(int tablero_conBarcos[10][10], struct barco flota[], int num_barcos)
 {
-    
-
     for (int i = 0; i < num_barcos; i++)
     {
+        // printf("numeros barcos %d\n", num_barcos);
+        // printf("Barcos restantes %d\n", num_barcos - i);
         for (int j = 0; j < flota[i].cantidad; j++)
         {
-            int correctoFila = 0;
-            int correctoColumna = 0;
+            // printf("nombre de barco: %s, cantidad total: %d, cantidad restante: %d\n", flota[i].nombre, flota[i].cantidad, flota[i].cantidad - j);
+            int colocado = 0;
+            
+            while (!colocado) {
+                int fila = rand() % 10;
+                int columna = rand() % 10;
+                int orientacion = rand() % 2; // 0 = horizontal, 1 = vertical
+                int valido = 1;
 
-            int fila = 0;
-            int columna = 0;
-            int orientacion = 0;
-            do
-            {
-
-                fila = rand() % 10;
-                columna = rand() % 10;
-                orientacion = rand() % 2; // 0 = horizontal, 1 = vertical
-
-                if (orientacion == 1)
+                if (orientacion == 0)
                 {
-                    if (fila + flota[i].casillas <= 10)
+                    // Comprovar si cabe el barco en el tablero
+                    if (columna + flota[i].casillas > 10)
                     {
-                        correctoFila = 1;
+                        valido = 0;
+                    }
+                    else
+                    {
+                        for (int k = 0; k < flota[i].casillas; k++)
+                        {
+                            if (tablero_conBarcos[fila][columna + k] != 0)
+                            {
+                                valido = 0;
+                                break;
+                            }
+                        }
                     }
                 }
                 else
                 {
-                    if (columna + flota[i].casillas <= 10)
+                    if (fila + flota[i].casillas > 10)
                     {
-                        correctoColumna = 1;
+                        valido = 0;
+                    }
+                    else
+                    {
+                        for (int k = 0; k < flota[i].casillas; k++)
+                        {
+                            if (tablero_conBarcos[fila + k][columna] != 0)
+                            {
+                                valido = 0;
+                                break;
+                            }
+                        }
                     }
                 }
 
-            } while (correctoFila == 0 && correctoColumna == 0);
-
-            if (orientacion == 0)
-            {
-                // Horizontal
-                for (int k = 0; k < flota[i].casillas; k++)
+                if (valido)
                 {
-                    if (tablero_conBarcos[][] != 0) {
-                        
+                    if (orientacion == 0)
+                    {
+                        for (int k = 0; k < flota[i].casillas; k++)
+                            tablero_conBarcos[fila][columna + k] = 1;
+                            printf("barco %s, colocado\n", flota[i].nombre);
                     }
-                    tablero_conBarcos[fila][columna + k] = 1;
-                }
-            }
-            else
-            {
-                // vertical
-                for (int p = 0; p < flota[i].casillas; p++)
-                {
-                    tablero_conBarcos[fila + p][columna] = 1;
+                    else
+                    {
+                        for (int k = 0; k < flota[i].casillas; k++)
+                            tablero_conBarcos[fila + k][columna] = 1;
+                        printf("barco %s, colocado\n", flota[i].nombre);
+                    }
+                    colocado = 1;
                 }
             }
         }
@@ -95,6 +110,12 @@ void tablero_barcos(int tablero_conBarcos[10][10], struct barco flota[], int num
         }
         printf("\n");
     }
+}
+
+void entradaUser(const char *mensaje, chat *User) {
+    printf("%s", mensaje);
+
+    
 }
 
 int main(void)
@@ -125,48 +146,67 @@ int main(void)
     int tablero_conBarcos[10][10] = {0};
 
     int num_flota1 = sizeof(flota1) / sizeof(flota1[0]);
+    int num_flota2 = sizeof(flota2) / sizeof(flota2[0]);
+    int num_flota3 = sizeof(flota3) / sizeof(flota3[0]);
 
+    // Tablero sin barcos
     // printar_tablero(tablero);
-    tablero_barcos(tablero_conBarcos, flota1, num_flota1);
 
-    // int num_tipos = sizeof(flota1) / sizeof(flota1[0]);
+    // Tablero con barcos
+    // tablero_barcos(tablero_conBarcos, flota1, num_flota1); //Se muestra correctamente
 
-    // for (int i = 0; i < num_tipos; i++)
-    // {
-    //     printf("Tipo: %s\n", flota1[i].nombre);
-    //     printf("Cantidad: %d\n", flota1[i].cantidad);
-    //     printf("Casillas: %d\n", flota1[i].casillas);
-    // }
+    int hecho = 0;  
 
-    // int hecho = 0;
+    do{
+        printf("┌──────────────────────────────────┐\n");
+        printf("│        Escull una flota          │\n");
+        printf("├──────────────────────────────────┤\n");
+        printf("│ 1 - L'Armada Vencible            │\n");
+        printf("│ 2 - Els Brivalls                 │\n");
+        printf("│ 3 - La Flotilla                  │\n");
+        printf("└──────────────────────────────────┘\n");
+        printf("Introdueix el número de la flota (1-3): ");
 
-    // do{
-    //     printf("┌──────────────────────────────────┐\n");
-    //     printf("│        Escull una flota          │\n");
-    //     printf("├──────────────────────────────────┤\n");
-    //     printf("│ 1 - L'Armada Vencible            │\n");
-    //     printf("│ 2 - Els Brivalls                 │\n");
-    //     printf("│ 3 - La Flotilla                  │\n");
-    //     printf("└──────────────────────────────────┘\n");
-    //     printf("Introdueix el número de la flota (1-3): ");
+        int opcionCorrecta = 0;
+        int intValue;
+        while (!opcionCorrecta)
+        {
+            scanf("%d", &intValue);
+            if (intValue >= 1 && intValue <= 3)
+            {
+                switch (intValue)
+                {
+                case 1:
+                    printar_tablero(tablero);
+                    tablero_barcos(tablero_conBarcos, flota1, num_flota1);
+                    break;
 
-    //     while (!hecho)
-    //     {
-    //         scanf("%d", &intValue);
-    //         if (intValue >= 1 && intValue <= 3)
-    //         {
+                case 2:
+                    printar_tablero(tablero);
+                    tablero_barcos(tablero_conBarcos, flota2, num_flota2);
+                    break;
 
-    //             hecho = 1;
-    //         }
-    //         else
-    //         {
-    //             printf("Escull la opció 1, 2 o 3: ");
-    //             while (getchar() != '\n')
-    //                 ;
-    //         }
-    //     }
-    // } while (condition)
-    // {
-    //     /* code */
-    // }
+                case 3:
+                    printar_tablero(tablero);
+                    tablero_barcos(tablero_conBarcos, flota3, num_flota3);
+                    break;
+
+                default:
+                    break;
+                }
+                opcionCorrecta = 1;
+            }
+            else
+            {
+                printf("Escull la opció 1, 2 o 3: ");
+                while (getchar() != '\n')
+                    ;
+            }
+        }
+
+    } while (hecho == 0);
+    {
+         while (getchar() != '\n')
+                    ;
+    }
 }
